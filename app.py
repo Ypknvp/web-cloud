59e2268aa28e311571c09cf10beb18b18361959a
Ypknvp <149500393+Ypknvp@users.noreply.github.com>
2024-09-01 18:13:46 +0530

    app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 import os
+from bson.objectid import ObjectId  # Import ObjectId for deletion
 
 app = Flask(__name__)
 
@@ -37,3 +38,4 @@ if __name__ == '__main__':
     # Use the PORT environment variable provided by Render
     port = int(os.getenv('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
