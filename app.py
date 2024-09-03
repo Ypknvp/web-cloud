7313fd98d84995b00a2f28e4c5e0317e5927e9d1
Ypknvp <149500393+Ypknvp@users.noreply.github.com>
2024-09-03 12:29:30 +0530

    app.py

--- app.py
+++ app.py
@@ -1,41 +1,46 @@
 from flask import Flask, render_template, request, redirect, url_for
-from pymongo import MongoClient
+from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-import os
-from bson.objectid import ObjectId  # Import ObjectId for deletion
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-# MongoDB Atlas connection
-uri = os.getenv('MONGO_URI')
+# MongoDB connection string
+uri = "mongodb+srv://yogeshp:j22nsWPUUN0uKj9L@cluster0.0six4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = MongoClient(uri, server_api=ServerApi('1'))
-db = client['test']  # Replace 'test' with your database name
+db = client['name_database']
 collection = db['names']
 
+# Display names and form
 @app.route('/')
 def index():
     names = collection.find()
     return render_template('index.html', names=names)
 
+# Add a new name
 @app.route('/add_name', methods=['POST'])
 def add_name():
-    name = request.form['name']
-    collection.insert_one({'name': name})
+    name = request.form.get('name')
+    if name:
+        collection.insert_one({'name': name})
     return redirect(url_for('index'))
 
-@app.route('/delete_name/<name_id>')
-def delete_name(name_id):
-    collection.delete_one({'_id': ObjectId(name_id)})
-    return redirect(url_for('index'))
+# Edit a name
+@app.route('/edit_name/<id>', methods=['GET', 'POST'])
+def edit_name(id):
+    if request.method == 'POST':
+        new_name = request.form.get('name')
+        if new_name:
+            collection.update_one({'_id': ObjectId(id)}, {"$set": {'name': new_name}})
+        return redirect(url_for('index'))
+    name = collection.find_one({'_id': ObjectId(id)})
+    return render_template('edit.html', name=name)
 
-@app.route('/edit_name/<name_id>', methods=['POST'])
-def edit_name(name_id):
-    new_name = request.form['name']
-    collection.update_one({'_id': ObjectId(name_id)}, {'$set': {'name': new_name}})
+# Delete a name
+@app.route('/delete_name/<id>', methods=['GET'])
+def delete_name(id):
+    collection.delete_one({'_id': ObjectId(id)})
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    # Use the PORT environment variable provided by Render
-    port = int(os.getenv('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
-
+    app.run(debug=True)
